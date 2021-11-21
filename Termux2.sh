52e0e11fbecd999dcc6997b7cb85bfe396afd7f2
Ahmed Khalaf <59212055+Ahmedkhalaf477@users.noreply.github.com>
2021-11-21 14:21:38 +0200

    Add files via upload

---
+++ Termux2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo " Welcome To My The Worled "
+echo " By Ahmed_khalaf  "
+echo " https://www.facebook.com/Ahmedkhkh477"
+echo ================================
+pkg update -y
+
+pkg upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install ruby -y
+
+pkg install git -y
+
+pkg install php -y
+
+pkg install java -y
+
+pkg install bash -y
+
+pkg install perl -y
+
+pkg install nmap -y
+
+pkg install bash -y
+
+pkg install clang -y
+
+pkg install nano -y
+
+pkg install zip -y
+
+pkg install unzip -y
+
+pkg install tor -y
+
+pkg install sudo -y
+
+pkg install wget -y
+
+pkg install openssl -y
+echo ===============================
+echo " Welcome To My The Worled "
+echo " By Ahmed_khalaf.. Don..! "
+echo " https://www.facebook.com/Ahmedkhk>
+echo ================================
